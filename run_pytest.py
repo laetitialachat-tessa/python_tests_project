c9bb8ad9dc9bc57557ac4ffd4b0846585d35cef4
laetitialachat-tessa <laetitialachat@tessatherapeutics.com>
2018-05-08 16:43:59 +0800

    Add a script for running pytest

---
+++ run_pytest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 16:28:48 2018
+
+@author: laetitialachat
+"""
+
+import pytest
+
+#pytest.main()
+pytest.main(['--junitxml=results_xml/tests_output.xml'])
